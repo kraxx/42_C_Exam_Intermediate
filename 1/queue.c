dae3cfef44c098cfacc92b3295fba290e93b3545
jchow <jchow@student.42.us.org>
2018-02-21 15:27:27 -0800

    queue && stack

---
+++ 1/queue.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+
+typedef struct s_node {
+    void          *content;
+    struct s_node *next;
+} Node;
+
+typedef struct s_queue {
+    struct s_node *first;
+    struct s_node *last;
+} Queue;
+
+
+Queue* init(void) {
+	Queue* q = malloc(sizeof(Queue));
+	q->first = 0;
+	q->last = 0;
+	return q;
+}
+
+void enqueue(struct s_queue *queue, void *content) {
+	Node* new = malloc(sizeof(Node));
+	new->content = content;
+	new->next = 0;
+	if (!queue->first)
+		queue->first = new;
+	if (queue->last)
+		queue->last->next = new;
+	queue->last = new;
+}
+
+void* dequeue(struct s_queue *queue) {
+	if (!queue->first)
+		return 0;
+	Node* tmp = queue->first;
+	void* content = tmp->content;
+	queue->first = tmp->next;
+	if (!queue->first)
+		queue->last = 0;
+	free(tmp);
+	return content;
+}
+
+void* peek(struct s_queue *queue) {
+	if (!queue->first)
+		return 0;
+	return queue->content;
+}
+
+int isEmpty(struct s_queue *queue) {
+	if (!queue->first)
+		return 1;
+	return 0;
+}
+
+
+/*
+QUEUE
+Assignment name  : queue
+Expected files   : queue.c
+Allowed functions: malloc free
+--------------------------------------------------------------------------------
+Implement a queue data structure in C, using the following structures in your 
+code:
+    struct s_node {
+        void *content;
+        struct s_node *next;
+    };
+    struct s_queue {
+        struct s_node *first;
+        struct s_node *last;
+    };
+A queue uses FIFO (first-in fist-out) ordering :
+items are removed from the data structure in the same order that they are added.
+Implement 5 functions for the following queue operations :
+    - init() : Initialize the queue.
+      The first and last pointers are set to NULL.
+    - enqueue(queue, item) : Add an item to the end of the queue.
+    - dequeue(queue) : Remove the first item from the queue and return it.
+      If the queue is empty, the function returns NULL.
+    - peek(queue) : Return the first item of the queue.
+      If the queue is empty, the function returns NULL.
+    - isEmpty(queue) : Return 1 if the queue is empty, 0 otherwise.
+These functions must be declared as follows:
+    struct s_queue *init(void);
+    void enqueue(struct s_queue *queue, void *content);
+    void *dequeue(struct s_queue *queue);
+    void *peek(struct s_queue *queue);
+    int isEmpty(struct s_queue *queue);
+*/
---
+++ 1/stack.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+
+typedef struct s_node {
+    void          *content;
+    struct s_node *next;
+} Node;
+
+typedef struct s_stack {
+    struct s_node *top;
+} Stack;
+
+Stack* init(void) {
+	Stack* s = malloc(sizeof(Stack));
+	s->top = 0;
+	return s;
+}
+
+void* pop(struct s_stack *stack) {
+	if (!stack->top)
+		return 0;
+	Node* tmp = stack->top;
+	void* content = tmp->content;
+	stack->top = tmp->next;
+	free(tmp);
+	return content;
+}
+
+void push(struct s_stack *stack, void *content) {
+	Node* new = malloc(sizeof(Node));
+	new->content = content;
+	new->next = stack->top;
+	stack->top = new;
+}
+
+void *peek(struct s_stack *stack) {
+	if (!stack->top)
+		return 0;
+	return stack->top->content;
+}
+
+int isEmpty(struct s_stack *stack) {
+	if (!stack->top)
+		return 1;
+	return 0;
+}
+
+#include <stdio.h>
+int main() {
+
+	Stack* s = init();
+
+	printf("Empty? %d\n", isEmpty(s));
+	puts("Pushing 'Hello'");
+	push(s, "Hello");
+	printf("Empty? %d\n", isEmpty(s));
+	printf("Peeking: %s\n", peek(s));
+	puts("Pushing 'World'");
+	push(s, "World");
+	printf("Empty? %d\n", isEmpty(s));
+	printf("Peeking: %s\n", peek(s));
+	puts("Pushing ':)'");
+	push(s, ":)");
+	printf("Empty? %d\n", isEmpty(s));
+	printf("Peeking: %s\n", peek(s));
+
+	printf("Popped: %s\n", pop(s));
+	printf("Popped: %s\n", pop(s));
+	printf("Popped: %s\n", pop(s));
+	printf("Popped: %s\n", pop(s));
+	printf("Empty? %d\n", isEmpty(s));
+}
+
+/*
+STACK
+Assignment name  : stack
+Expected files   : stack.c
+Allowed functions: malloc free
+--------------------------------------------------------------------------------
+Implement a stack data structure in C, using the following structures in your
+code:
+    struct s_node {
+        void          *content;
+        struct s_node *next;
+    };
+    struct s_stack {
+        struct s_node *top;
+    };
+A stack uses LIFO (last-in fist-out) ordering :
+the most recent item added to the stack is the first item to be removed.
+Implement 5 functions for the following stack operations :
+    - init() : Initialize the stack.
+      The top pointer is set to NULL.
+    - pop(stack) : Remove the top item from the stack and return it.
+      If the stack is empty, the function returns NULL.
+    - push(stack, item) : Add an item to the top of the stack.
+    - peek(stack) : Return the top of the stack.
+      If the stack is empty, the function returns NULL.
+    - isEmpty(stack) : Return 1 if the stack is empty, 0 otherwise.
+These functions must be declared as follows:
+    struct s_stack *init(void);
+    void *pop(struct s_stack *stack);
+    void push(struct s_stack *stack, void *content);
+    void *peek(struct s_stack *stack);
+    int isEmpty(struct s_stack *stack);
+*/
