18dfd7f15c67101e56307aed8c6e0945745cff19
jchow <jchow@student.42.us.org>
2018-02-20 23:55:46 -0800

    is_looping done

---
+++ 2/is_looping.c
@@ -0,0 +1,73 @@
+typedef struct s_node {
+	int value;
+	struct s_node *next;
+} Node;
+
+int is_looping(struct s_node *node) {
+	Node* n1 = node;
+	Node* n2 = node;
+
+	while (*n1 && *n2) {
+
+		if (n1->next)
+			n1 = n1->next;
+		if (n1->next)
+			n1 = n1->next;
+		if (n2->next)
+			n2 = n2->next;
+		if (n1 == n2)
+			return 1;
+
+	}
+	return 0;
+}
+
+/*
+IS_LOOPING
+Assignment name  : is_looping
+Expected files   : is_looping.c
+Allowed functions: 
+--------------------------------------------------------------------------------
+ALERT: OPTIMIZED SOLUTION REQUIRED.
+Given the first node of a linked list as parameter, create a function which
+returns 1 if the linked list is looping, otherwise 0.
+The linked list uses the following structure:
+struct s_node {
+    int value;
+    struct s_node *next;
+};
+The function must be declared as follows:
+int is_looping(struct s_node *node);
+Considerations:
+- Be careful: the naive solution won't work on our big input, you have to
+find a solution with better complexity than O(N^2) time (where N is the
+number of nodes).
+- The values of each node does not matter.
+- The bigger test we will do is on a linked list of 500.000 nodes, with the
+beginning of the loop at the middle. It should run in less than 2 seconds.
+Example 1:
+1 -> 2 -> 3 -> 4 -> 5
+          ^         |
+          |         v
+          |         6
+           \        |
+            \______/
+In this case, it should return 1 (at the node 3 begins the loop).
+Example 2: 
+12 -> 150 -> 30 -> 50 -> 345 -> 120
+       ^                         |
+       |                         v
+       |                        200
+        \                      /
+         \____________________/
+In this case, it should return 1 (the loop begins at node 150).
+Example 3:
+12 -> 150 -> 30 -> 50 -> 345 -> 120
+In this case, it should return 0 (no loop begins).
+Example 4:
+12 -> 19 -> 14
+            ^ \
+            | |
+            \/
+In this case, it should return 1 (the loop begins at node 14).
+*/
