3bc2bda68ba184ae80c501ba3a05b5e7c418cf63
jchow <jchow@student.42.us.org>
2018-03-04 23:10:00 -0800

    incomplete infin_mult

--- 5/infin_mult.c
+++ 5/infin_mult.c
@@ -1,3 +1,87 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdio.h>
+
+int myStrLen(char* s) {
+	int ret = 0;
+	while (*s++)
+		ret++;
+	return ret;
+}
+
+int main(int ac, char* av[]) {
+	if (ac != 3)
+		return 0;
+
+	char* s1 = av[1];
+	char* s2 = av[2];
+	int neg = 0;
+
+	if (*s1 == '-' && *s2 == '-') {
+		s1++;
+		s2++;
+	} else if (*s1 == '-' && *s2 != '-') {
+		s1++;
+		neg = 1;
+	} else if (*s1 != '-' && *s2 == '-') {
+		s2++;
+		neg = 1;
+	}
+
+	int len1 = myStrLen(s1);
+	int len2 = myStrLen(s2);
+
+	printf("lens %d %d\n", len1, len2);
+	int ret[len1 + len2];
+	for (int i = 0; i < len1 + len2; i++)
+		ret[i] = 0;
+
+	int idx1 = 0, idx2 = 0;
+	for (int i = len1 - 1; i >= 0; i--) {
+
+		int carry = 0;
+		int n1 = s1[i] - '0';
+
+		idx2 = 0;
+		for (int j = len2 - 1; j >= 0; j--) {
+
+			int n2 = s2[j] - '0';
+			int sum = (n1 * n2) + ret[idx1 + idx2] + carry;
+			carry = sum / 10;
+			ret[idx1 + idx2] = sum % 10;
+			idx2++;
+
+			printf("ret: %d\n", ret[idx1 + idx2]);
+		}
+
+		while (carry > 0) {
+			ret[idx1 + idx2] += carry % 10;
+			carry /= 10;
+			idx2++;
+		}
+
+		idx1++;
+	}
+
+	int idx = idx1 + idx2;
+	while (idx >= 0 && ret[idx] == 0)
+		idx--;
+	if (idx == -1) {
+		write(1, "0\n", 2);
+		return 0;
+	}
+	if (neg)
+		write(1, "-", 1);
+	while (idx >= 0) {
+		char x = ret[idx] + '0';
+		write(1, &x, 1);
+		idx--;
+	}
+	write(1, "\n", 1);
+	return 0;
+}
+
 /*
 INFIN_MULT
 Assignment name  : infin_mult
