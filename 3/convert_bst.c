5b18b77d30aefc5c103e012bda8eb74fc77f87d9
jchow <jchow@student.42.us.org>
2018-03-04 18:11:34 -0800

    convert bst done

--- 3/convert_bst.c
+++ 3/convert_bst.c
@@ -1,3 +1,89 @@
+typedef struct s_node {
+        int           value;
+        struct s_node *right;
+        struct s_node *left;
+} Node;
+
+Node* recurse(Node* root) {
+
+	if (!root)
+		return 0;
+
+	if (root->left) {
+
+		Node* left = recurse(root->left);
+		while (left->right)
+			left = left->right;
+		left->right = root;
+		root->left = left;
+
+	}
+	if (root->right) {
+
+		Node* right = recurse(root->right);
+		while (right->left)
+			right = right->left;
+		right->left = root;
+		root->right = right;
+	}
+	return root;
+}
+
+Node* convert_bst(Node* bst) {
+	if (!bst)
+		return 0;
+	recurse(bst);
+	Node* left = bst;
+	Node* right = bst;
+	while (left->left)
+		left = left->left;
+	while (right->right)
+		right = right->right;
+	left->left = right;
+	right->right = left;
+	return left;
+}
+
+#include <stdlib.h>
+#include <stdio.h>
+Node* b(int v) {
+	Node* new = malloc(sizeof(Node));
+	new->value = v;
+	new->left = 0;
+	new->right = 0;
+	return new;
+}
+int main() {
+	Node* r = b(8);
+	r->left = b(4);
+	r->left->left = b(2);
+	r->left->left->left = b(1);
+	r->left->left->right = b(3);
+
+	r->left->right = b(6);
+	r->left->right->left = b(5);
+	r->left->right->right = b(7);
+
+	r->right = b(12);
+	r->right->left = b(10);
+	r->right->left->left = b(9);
+	r->right->left->right = b(11);
+	
+	r->right->right = b(14);
+	r->right->right->left = b(13);
+	r->right->right->right = b(15);
+
+	r = convert_bst(r);
+	for (int i = 0; i < 15; i++) {
+		printf("%d\n", r->value);
+		r = r->right;
+	}
+	for (int i = 0; i < 17; i++) {
+		printf("%d\n", r->value);
+		r = r->left;
+	}
+}
+
 /*
 CONVERT_BST
 Assignment name  : convert_bst
