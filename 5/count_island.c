9072e07387520e8781732fdc127f4217aac0ac4e
Justin Chow <jchow@Justins-iMac-2.local>
2018-03-05 22:10:24 -0800

    count_island done

--- 5/count_island.c
+++ 5/count_island.c
@@ -1,3 +1,61 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void floodFill(char buf[100000], int size, int width, int height, int i, char replacer) {
+
+	buf[i] = replacer;
+
+	if ((i > width) && (buf[i - width] == 'X'))
+		floodFill(buf, size, width, height, i - width, replacer);
+
+	if ((i < size - width - 1) && buf[i + width] == 'X')
+		floodFill(buf, size, width, height, i + width, replacer);
+
+	if (i && (i % width > 0) && buf[i - 1] == 'X')
+		floodFill(buf, size, width, height, i - 1, replacer);
+
+	if (i && (i % width < width - 1) && buf[i + 1] == 'X')
+		floodFill(buf, size, width, height, i + 1, replacer);
+}
+
+void countIsland(char* file) {
+	
+	char buf[100000] = {0};
+	int fd = open(file, O_RDONLY);
+
+	int size = read(fd, buf, 100000);
+	if (size < 0) {
+		write(1, "\n", 1);
+		return;
+	}
+
+	int width = 1;
+	for (int i = 0; buf[i] != '\n'; i++)
+		width++;
+
+	int height = size / width;
+	if (size / height != width) {
+		write(1, "\n", 1);
+		return;
+	}
+
+	char replacer = '0';
+	for (int i = 0; i < size; i++) {
+		if (buf[i] == 'X') {
+			floodFill(buf, size, width, height, i, replacer);
+			replacer++;
+		}
+	}
+	write(1, buf, size);
+}
+
+int main(int ac, char* av[]) {
+	if (ac == 2)
+		countIsland(av[1]);
+	return 0;
+}
+
 /*
 COUNT_ISLAND
 Assignment name  : count_island
